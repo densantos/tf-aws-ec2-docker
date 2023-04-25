7e36ebd21f7b520b170844ff2507ca30bce260a0
Denis Santos <denis.santos@brambles.com>
2023-04-25 15:29:18 -0700

    Docker installation

--- instance.tf
+++ instance.tf
@@ -3,6 +3,29 @@ resource "aws_instance" "ec2" {
   instance_type = "t2.large"
   key_name = "dsantos-labs"
   iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
+
+  user_data = <<EOF
+#!/bin/bash
+        
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg
+
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg    
+
+echo \
+"deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+EOF
  
 network_interface {
     network_interface_id = aws_network_interface.nic.id
